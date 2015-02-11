f8e8a9d12152d78ea026c1e3eea13c7addda9619
calesanz <calesanz@gmail.com>
2015-02-11 13:36:36 +0100

    replaced user.anzeigen.php with user.list and user.detail

--- view/user.anzeigen.php
+++
@@ -1,8 +0,0 @@
-<ul>
-<?php 
-	foreach($users as $user){
-		echo "<li>ID: '$user->id' <br> Username: '$user->username' <br> Email: '$user->email' <br> Compare String: ' $user->compareString '</li>";
-	}
-?>
-
-</ul>
