790e66381b230fcb6203b9cee40076d88e689264
calesanz <calesanz@gmail.com>
2015-02-24 23:07:45 +0100

    Added foundation -> a css/js framework

--- view/gaestebuch.anzeigen.php
+++ view/gaestebuch.anzeigen.php
@@ -1,5 +1,29 @@
-<p><?php 
-foreach($entries as $entry){
-		echo "<li> $entry->User->Username : $entry->Text </li>";
-	}
-?></p>
+
+<ul>
+<?php
+foreach ( $entries as $entry ) {
+	?>
+	<div class="row">
+		<div class="large-2 columns small-3">
+			<img src="http://placehold.it/80x80&text=[img]" />
+		</div>
+		<div class="large-10 columns">
+			<div class="panel">
+	<?php
+	echo "<strong>[ $entry->CreatedAt ] " . $entry->User->FullName . ": </strong>";
+	echo "<p>$entry->Text </p>";
+	?>
+							
+			</div>
+			<ul class="inline-list">
+				<li><a href="">Reply</a></li>
+				<li><a href="">Share</a></li>
+			</ul>
+
+		</div>
+	</div>
+	
+	<?php
+}
+?>
+</ul>
--- view/gaestebuch.php
+++ view/gaestebuch.php
@@ -1,10 +1,72 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="utf-8">
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Guestbook</title>
+<link rel="stylesheet" href="css/foundation.css" />
+<script src="js/vendor/modernizr.js"></script>
 </head>
 <body>
-	<h1><?php echo $title; ?></h1>
-		<div class ="content"><?php $innercontent->display()?></div>
-	</body>
+	<!-- Titlebar -->
+	<div class="row">
+		<div class="large-12 columns">
+			<div class="panel">
+				<h1><?php echo $title; ?></h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+
+		<div class="large-8 columns">
+			<!-- Main Content -->
+			<div class="row">
+				<div>
+					<?php $innercontent->display()?>
+				</div>
+
+
+
+			</div>
+		</div>
+		<aside class="large-4 columns ">
+			<form>
+				<input type="text"> <input type="text"> <input type="text">
+			</form>
+		</aside>
+
+	</div>
+
+	<footer class="row">
+		<div class="large-12 columns">
+			<hr />
+			<div class="row">
+				<div class="large-5 columns">
+					<p>&copy; Copyright no one at all. Go to town.</p>
+				</div>
+				<div class="large-7 columns">
+					<ul class="inline-list right">
+						<li><a href="#">Section 1</a></li>
+						<li><a href="#">Section 2</a></li>
+						<li><a href="#">Section 3</a></li>
+						<li><a href="#">Section 4</a></li>
+						<li><a href="#">Section 5</a></li>
+						<li><a href="#">Section 6</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
+
+
+		<script src="js/vendor/jquery.js"></script>
+		<script src="js/foundation.min.js"></script>
+		<script>
+      		$(document).foundation();
+    		</script>
+	</footer>
+
+</body>
 </html>
